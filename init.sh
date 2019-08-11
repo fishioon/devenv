340554771249ca0aeb14bd978e41778807191fa6
fish <fishioon@live.com>
2019-08-11 16:55:40 +0800

    add screenrc and improve

--- init.sh
+++ init.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 addLine() {
-	FILE=$1
-	LINE=$2
+	FILE=$1; LINE=$2
 	grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
+}
+
+exit_msg() {
+	echo $1; exit -1
 }
 
 [[ -z "$DEVENV_HOME" ]] && export DEVENV_HOME=$PWD
 [[ ! -d "$HOME/.config" ]] && mkdir -p $HOME/.config
 [[ ! -L "$HOME/.config/nvim" ]] && ln -sf $DEVENV_HOME/nvim $HOME/.config/nvim
 
-if [[ -z "$ZDOTDIR" ]]; then
-	echo "You need add $ZDOTDIR env first"
-else
-	addLine "$ZDOTDIR/.zshrc" "export DEVENV_HOME=\"$DEVENV_HOME\""
-	addLine "$ZDOTDIR/.zshrc" 'source $DEVENV_HOME/shrc'
-	[[ -f "$DEVENV_HOME/workrc" ]] && addLine "$ZDOTDIR/.zshrc" 'source $DEVENV_HOME/workrc'
-fi
+[[ -z "$ZDOTDIR" ]] && exit_msg "You need add [$ZDOTDIR] env first"
+addLine "$ZDOTDIR/.zshrc" "export DEVENV_HOME=\"$DEVENV_HOME\""
+addLine "$ZDOTDIR/.zshrc" 'source $DEVENV_HOME/shrc'
+[[ -f "$DEVENV_HOME/workrc" ]] && addLine "$ZDOTDIR/.zshrc" 'source $DEVENV_HOME/workrc'
 
 echo "🎉 🎉 Good luck"
