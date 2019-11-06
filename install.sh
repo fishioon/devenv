f83594e011092e1a1688c47de9d27a3a6c32942b
ifish <fishioon@live.com>
2019-11-06 15:25:26 +0800

    fix

--- install.sh
+++ install.sh
@@ -4,20 +4,16 @@ add_line() {
 	FILE=$1; LINE=$2
 	grep -qxF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
 }
-
-exit_msg() {
-	echo $1; exit -1
-}
-
-[ -z "$DEVENV_HOME" ] && export DEVENV_HOME=$PWD; add_line "$HOME/.zshenv" "export DEVENV_HOME=\"$DEVENV_HOME\""
-[ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"; add_line "$HOME/.zshenv" "export XDG_CONFIG_HOME=\"$XDG_CONFIG_HOME\""
-[ -z "$ZDOTDIR" ] && export ZDOTDIR="$XDG_CONFIG_HOME/zsh"; add_line "$HOME/.zshenv" "export ZDOTDIR=\"$ZDOTDIR\""
+zshenv_file=$HOME/.zshenv
+[ -z "$DEVENV" ] && export DEVENV=$PWD; add_line $zshenv_file "export DEVENV=\"$DEVENV\""
+[ -z "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"; add_line $zshenv_file "export XDG_CONFIG_HOME=\"$XDG_CONFIG_HOME\""
+[ -z "$ZDOTDIR" ] && export ZDOTDIR="$XDG_CONFIG_HOME/zsh"; add_line $zshenv_file "export ZDOTDIR=\"$ZDOTDIR\""
 
 [ ! -d "$XDG_CONFIG_HOME" ] && mkdir -p $HOME/.config
 
-[ ! -L "$ZDOTDIR" ] && ln -sf $DEVENV_HOME/zsh $XDG_CONFIG_HOME/zsh
-[ ! -L "$XDG_CONFIG_HOME/nvim" ] && ln -sf $DEVENV_HOME/nvim $XDG_CONFIG_HOME/nvim
-[ ! -L "$XDG_CONFIG_HOME/git" ] && ln -sf $DEVENV_HOME/git $XDG_CONFIG_HOME/git
+[ ! -L "$ZDOTDIR" ] && ln -sf $DEVENV/zsh $XDG_CONFIG_HOME/zsh
+[ ! -L "$XDG_CONFIG_HOME/nvim" ] && ln -sf $DEVENV/nvim $XDG_CONFIG_HOME/nvim
+[ ! -L "$XDG_CONFIG_HOME/git" ] && ln -sf $DEVENV/git $XDG_CONFIG_HOME/git
 
 ### add
 command -v fzf || (brew install fzf && $(brew --prefix)/opt/fzf/install --xdg --no-bash --no-fish --no-update-rc --key-bindings --completion)
