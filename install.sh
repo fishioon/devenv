d98a4a430efe4b6a6a602c83194da95bde6fa4bb
fish <fishioon@live.com>
2020-04-01 16:42:48 +0800

    update

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ source $HOME/.zshenv
 
 [ ! -d "$XDG_CONFIG_HOME" ] && mkdir -p $XDG_CONFIG_HOME
 
+[ ! -L "$XDG_CONFIG_HOME/devenv" ] && ln -sf "$PWD/$i" "$XDG_CONFIG_HOME/devent"
 for i in zsh nvim git; do
 	[ ! -L "$XDG_CONFIG_HOME/$i" ] && ln -sf "$PWD/$i" "$XDG_CONFIG_HOME/$i"
 done
-
 
 ## fzf
 # command -v fzf || (brew install fzf && $(brew --prefix)/opt/fzf/install --xdg --no-bash --no-fish --no-update-rc --key-bindings --completion)
